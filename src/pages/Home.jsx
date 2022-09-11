2d97ddaa2761ba7370bc1f2109809b88a58471b3
Prem <premuix@gmail.com>
2022-09-11 12:08:01 +0530

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         >
 
           <NavbarStr />
-<div>ssssssssssd</div>
+<div>ssssssssssdc</div>
           <section className="py-80">
             <div className="container">
               <div className="row gx-5">
