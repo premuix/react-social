b7a232f6b2593da94010ea168fe24fc9431b3838
Prem <premuix@gmail.com>
2022-09-12 19:32:12 +0530

    add

--- src/components/NavbarStr.jsx
+++ src/components/NavbarStr.jsx
@@ -26,10 +26,10 @@ const Navbar= () => {
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                    <NavLink className="nav-link active" aria-current="page" to="/Home">Home</NavLink>
+                    <NavLink className="nav-link" activeClassName="active" to="/Home">Home</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" aria-current="page" to="/About">About Us</NavLink>
+                    <NavLink className="nav-link"  to="/About">About Us</NavLink>
                   </li>
                   <li className="nav-item">
                     <NavLink className="nav-link" to="/Services">Services</NavLink>
