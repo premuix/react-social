a6d25783dab82533546fbadfaac313d513c5d052
Prem <premuix@gmail.com>
2022-09-12 19:20:35 +0530

    set active link

--- src/components/NavbarStr.jsx
+++ src/components/NavbarStr.jsx
@@ -26,7 +26,7 @@ const Navbar= () => {
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                    <NavLink className="nav-link" aria-current="page" to="/Home">Home</NavLink>
+                    <NavLink className="nav-link active" aria-current="page" to="/Home">Home</NavLink>
                   </li>
                   <li className="nav-item">
                     <NavLink className="nav-link" aria-current="page" to="/About">About Us</NavLink>
